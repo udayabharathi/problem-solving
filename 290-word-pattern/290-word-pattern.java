defa44d368d9a27f8167e5e77305e59aa2c162ea
udayabharathi <35050065+udayabharathi@users.noreply.github.com>
2022-01-17 22:13:46 +0530

    Time: 7 ms (5.75%), Space: 38.8 MB (15.00%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        Map<String, Character> reverseMap = new HashMap<>();
+        String[] words = s.split("\\s+");
+        // System.out.println(Arrays.toString(words));
+        if (pattern.length() != words.length)
+            return false;
+        int index = 0;
+        for (char c : pattern.toCharArray()) {
+            if (reverseMap.containsKey(words[index]) && map.containsKey(c)) {
+                if (reverseMap.get(words[index]) != c || !map.get(c).equals(words[index]))
+                    return false;
+            } else if (reverseMap.containsKey(words[index]) || map.containsKey(c)) {
+                return false;
+            } else {
+                reverseMap.put(words[index], c);
+                map.put(c, words[index]);
+            }
+            index++;
+            // System.out.println("map: "+map+", reverseMap: "+reverseMap);
+        }
+        return true;
+    }
+}
