249815dfa00d9709315ea2ab187c24ddb3452683
udayabharathi <35050065+udayabharathi@users.noreply.github.com>
2022-08-03 09:42:32 +0530

    Time: 34 ms (65.32%), Space: 54.3 MB (65.61%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -9,26 +9,29 @@ class MyCalendar {
     public boolean book(int start, int end) {
         Map.Entry<Integer, Integer> floorEntry = map.floorEntry(start);
         Map.Entry<Integer, Integer> ceilingEntry = map.ceilingEntry(start);
-        if (floorEntry == null && ceilingEntry == null) {
-            map.put(start, end);
-            return true;
-        } else if (floorEntry == null && ceilingEntry.getKey() >= end) {
-            map.put(start, end);
-            return true;
-        } else if (ceilingEntry == null && floorEntry.getValue() <= start) {
-            map.put(start, end);
-            return true;
-        } else if (floorEntry != null && ceilingEntry != null && floorEntry.getValue() <= start && ceilingEntry.getKey() >= end) {
-            map.put(start, end);
-            return true;
-        } else {
+        if ((ceilingEntry != null && ceilingEntry.getKey() < end) || (floorEntry != null && floorEntry.getValue() > start)) {
             return false;
         }
+
+        map.put(start, end);
+        return true;
+        // if (floorEntry == null && ceilingEntry == null) {
+        //     map.put(start, end);
+        //     return true;
+        // } else if (floorEntry == null && ceilingEntry.getKey() >= end) {
+        //     map.put(start, end);
+        //     return true;
+        // } else if (ceilingEntry == null && floorEntry.getValue() <= start) {
+        //     map.put(start, end);
+        //     return true;
+        // } else if (floorEntry != null && ceilingEntry != null && floorEntry.getValue() <= start && ceilingEntry.getKey() >= end) {
+        //     map.put(start, end);
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     }
 }
-/*
-
-*/
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
